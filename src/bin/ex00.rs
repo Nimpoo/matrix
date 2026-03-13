e5b982b3de0e778f8aff605bfc4b6d1bf4067b50
Nimpoo <marwan.ayoub.pro@gmail.com>
2026-03-13 18:16:24 +0100

    Add basic tests for `Vector` to finished

--- src/bin/ex00.rs
+++ src/bin/ex00.rs
@@ -1,32 +1,48 @@
 use std::vec;
 
-use matrix;
+use matrix::{Vector, Matrix};
 
 fn main() {
-  let new: matrix::Vector<i32> = matrix::Vector::new();
-  println!("{:?}", new);
+  println!("|-------- Vector tests --------|");
 
-  let mut from: matrix::Vector<i32> = matrix::Vector::from(vec![1, 2]);
-  println!("{:?}", from);
+  println!();
 
-  from.push(4);
-  println!("{:?}", from);
+  println!("~ Basic functions");
 
-  from.pop();
-  println!("{:?}", from);
+  println!();
 
+  println!("1) `let vector_1: Vector<i32> = Vector::new()`");
+  let vector_1: Vector<i32> = Vector::new();
+  println!("variable `vector_1` = [ {:?} ]", vector_1);
 
-  let mat_1: matrix::Matrix<i32> = matrix::Matrix::from(vec![
-    vec![1, 2],
-    vec![3, 4],
-  ]);
+  println!();
 
-  println!("{:?}", mat_1);
+  println!("2) `let mut vector_1: Vector<i32> = Vector::from(4)`");
+  let mut vector_1: Vector<i32> = Vector::from(vec![4, 2]);
+  println!("variable `vector_1` = [ {:?} ]", vector_1);
 
-  let mat_2: matrix::Matrix<i32> = matrix::Matrix::from(vec![
-    vec![5, 6],
-    vec![7, 8]
-  ]);
+  println!();
 
-  
+  println!("3) `vector_1.push(1)`");
+  vector_1.push(1);
+  println!("Value `1` pushed in variable `vector_1` = [ {:?} ]", vector_1);
+
+  println!();
+
+  println!("4) `vector_1.pop()`");
+  vector_1.pop();
+  println!("Last value poped in variable `vector_1` = [ {:?} ]", vector_1);
+
+  println!();
+
+  println!("~ Mandatory functions");
+
+  println!();
+
+  println!("1) `vector_1.add(&vector_2)`");
+  let vector_2: Vector<i32> = Vector::from(vec![1, 1]);
+  let addition_vector_1: Result<(), &str> = vector_1.add(&vector_2);
+  if addition_vector_1.unwrap() == {
+      // TODO: do a match-case here for this test
+  }
 }
